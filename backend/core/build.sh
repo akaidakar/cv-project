0d28c9d8bd334e6533f5563a2f81cdb062b14e63
akaidakar <avadavasya@gmail.com>
2024-10-15 13:08:57 +0600

    build.sh fix

--- backend/core/build.sh
+++ backend/core/build.sh
@@ -13,19 +13,6 @@ echo "Attempting to run migrations..."
 python manage.py migrate 2>&1 | tee migrations.log
 echo "Migrations completed."
 
-#echo "Attempting to create superuser..."
-#if [ -z "$DJANGO_SUPERUSER_USERNAME" ] || [ -z "$DJANGO_SUPERUSER_EMAIL" ] || [ -z "$DJANGO_SUPERUSER_PASSWORD" ]; then
-    #echo "Error: DJANGO_SUPERUSER_USERNAME, DJANGO_SUPERUSER_EMAIL, or DJANGO_SUPERUSER_PASSWORD environment variable is not set." | tee -a superuser.log
-#else
-    #python manage.py createsuperuser --noinput 2>&1 | tee superuser.log
-    #if [ $? -eq 0 ]; then
-        #echo "Superuser created successfully." | tee -a superuser.log
-    #else
-        #echo "Failed to create superuser. Check the logs for more details." | tee -a superuser.log
-    #fi
-#fi
-echo "Superuser creation attempt completed."
-
 echo "Build process completed."
 
 # Print out all logs for debugging
@@ -35,5 +22,3 @@ echo "=== COLLECTSTATIC LOG ==="
 cat collectstatic.log
 echo "=== MIGRATIONS LOG ==="
 cat migrations.log
-echo "=== SUPERUSER CREATION LOG ==="
-cat superuser.log
