cc8dfcf3860ed9489bb382f8093d084a3513b11a
akaidakar <avadavasya@gmail.com>
2024-10-15 11:24:39 +0600

    bug fix

--- backend/core/build.sh
+++ backend/core/build.sh
@@ -5,6 +5,11 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 
+if [[$CREATE_SUPERUSER]];
+then
+    python manage.py createsuperuser --no-input
+fi
+
 # Remove the following lines:
 # python manage.py migrate
 # python manage.py search_index --rebuild -f
